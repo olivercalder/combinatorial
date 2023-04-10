388003e8ed930c73eb7a8c532394b6a06aa900e0
Oliver Calder <oliver@calder.dev>
2023-04-10 13:44:27 -0500

    Added triangle_number() function.
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,40 @@
 
 pub mod combinations;
 
+/// Returns the `n`th triangle number.
+///
+/// # Examples
+///
+/// ```
+/// use combinatorial::triangle_number;
+///
+/// assert_eq!(triangle_number(0), 0);
+/// assert_eq!(triangle_number(1), 1);
+/// assert_eq!(triangle_number(2), 3);
+///
+/// let nums: Vec<usize> = (3..8).map(triangle_number).collect();
+/// assert_eq!(nums, vec![6, 10, 15, 21, 28]);
+///
+/// assert_eq!(triangle_number(1000), (0..=1000).sum());
+/// ```
+pub fn triangle_number(n: usize) -> usize {
+    n * (n + 1) / 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use combinations::Combinations;
+
+    #[test]
+    fn test_triangle_number() {
+        assert_eq!(triangle_number(0), 0);
+        assert_eq!(triangle_number(1), 1);
+        assert_eq!(triangle_number(2), 3);
+        let nums: Vec<usize> = (3..8).map(triangle_number).collect();
+        assert_eq!(nums, vec![6, 10, 15, 21, 28]);
+        assert_eq!(triangle_number(1000), (0..=1000).sum());
+    }
 
     #[test]
     fn test_combinations_next() {
