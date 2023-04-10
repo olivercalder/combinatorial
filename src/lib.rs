71d6e08adb591b11d4290fa5daef96aeeb07f5ef
Oliver Calder <oliver@calder.dev>
2023-04-10 15:28:42 -0500

    Added factorial() and powerset(), adjusted modules
    Rather than exposing e.g. `Combinations` via the `combinations` module
    (i.e. `combinatorial::combinations::Combinations`), instead expose
    `combinatorial::Combinations` directly, and keep the `combinations`
    module non-public.
    
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! Combinatorial tools, functions, and generators.
 
-pub mod combinations;
+mod combinations;
+pub use combinations::Combinations;
 
 /// Returns the `n`th triangle number.
 ///
@@ -17,25 +18,60 @@ pub mod combinations;
 /// assert_eq!(nums, vec![6, 10, 15, 21, 28]);
 ///
 /// assert_eq!(triangle_number(1000), (0..=1000).sum());
+///
+/// for n in 1..=1000 {
+///     assert_eq!(triangle_number(n), n + triangle_number(n - 1));
+/// }
 /// ```
 pub fn triangle_number(n: usize) -> usize {
     n * (n + 1) / 2
 }
 
+/// Returns `n` factorial.
+///
+/// # Examples
+///
+/// ```
+/// use combinatorial::factorial;
+///
+/// assert_eq!(factorial(0), 1);
+/// assert_eq!(factorial(1), 1);
+/// assert_eq!(factorial(2), 2);
+/// assert_eq!(factorial(3), 6);
+///
+/// assert_eq!(factorial(10), (1..=10).product());
+///
+/// for n in 1..10 {
+///     assert_eq!(factorial(n), n * factorial(n - 1));
+/// }
+/// ```
+pub fn factorial(n: usize) -> usize {
+    (1..=n).product()
+}
+
+/// Returns the powerset of the elements in the given iterable.
+///
+/// # Examples
+///
+/// ```
+/// use combinatorial::powerset;
+///
+/// let mut subsets = powerset(vec!['x', 'y']);
+/// assert_eq!(subsets.next(), Some(Vec::new()));
+/// assert_eq!(subsets.next(), Some(vec!['x']));
+/// assert_eq!(subsets.next(), Some(vec!['y']));
+/// assert_eq!(subsets.next(), Some(vec!['x', 'y']));
+/// assert_eq!(subsets.next(), None);
+///
+/// assert_eq!(powerset(0..15).count(), 1 << 15);
+/// ```
+pub fn powerset<T: Ord + Clone>(elements: impl IntoIterator<Item = T>) -> Combinations<T> {
+    Combinations::all(elements)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use combinations::Combinations;
-
-    #[test]
-    fn test_triangle_number() {
-        assert_eq!(triangle_number(0), 0);
-        assert_eq!(triangle_number(1), 1);
-        assert_eq!(triangle_number(2), 3);
-        let nums: Vec<usize> = (3..8).map(triangle_number).collect();
-        assert_eq!(nums, vec![6, 10, 15, 21, 28]);
-        assert_eq!(triangle_number(1000), (0..=1000).sum());
-    }
 
     #[test]
     fn test_combinations_next() {
